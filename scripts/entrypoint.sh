af36e317538d527df99d0d41063d313300849d11
Jhon Bohorquez <jhon@btilab.co>
2024-04-25 00:48:16 -0500

    port

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,7 +7,7 @@ echo "$(date): Ejecutando proceso"
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 
-# python manage.py runserver 0.0.0.0:$PORT
+# python manage.py runserver 
+python manage.py runserver 0.0.0.0:$PORT
 
 
