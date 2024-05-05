5db095931f87d66cbe037790b4fd5cfbbbb83779
Jhon Bohorquez <jhon@btilab.co>
2024-05-04 19:40:38 -0500

    upgrade port docker

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,9 +5,15 @@ set -e
 echo "Contenedor iniciado"
 echo "$(date): Ejecutando proceso"
 
+# Obtener la dirección IP interna del contenedor
+CONTAINER_IP=$(hostname -I | awk '{print $1}')
+
+echo "La dirección IP del contenedor es: $CONTAINER_IP"
+
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py runserver 
+# python manage.py runserver 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:$PORT
+
 
 
